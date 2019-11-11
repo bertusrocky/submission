995b41a2e02b5dee1e16fe5be1deec1072da24e2
Adam Bertus <bertus.rocky@gmail.com>
2019-11-11 20:48:22 +0000

    ninety nine percent

--- main.py
+++ main.py
@@ -1,37 +1,57 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_required, current_user
 from .models import User, Submission
+from datetime import datetime
 from .import db
 
 
 main = Blueprint('main', __name__)
 
+# Render request page
 @main.route('/request')
 @login_required
 def submit():
   name = current_user.name
+  student_number = current_user.student_number
 
   requests = Submission.query.all()
 
+  return render_template('request.html', name=current_user.name, student_number=current_user.student_number, requests=requests)
 
-
-  return render_template('request.html', name=current_user.name, requests=requests)
-
-
+# Submit request
 @main.route('/request', methods=['POST'])
 @login_required
 def submit_post():
   title = request.form.get('title')
   author = request.form.get('author')
-  publication = request.form.get('year')
   isbn = request.form.get('isbn')
   campus = request.form.get('campus')
+  person = current_user.student_number
 
-  new_submission = Submission(title=title, author=author, publication=publication, isbn=isbn, campus=campus)
+
+  new_submission = Submission(title=title, author=author,isbn=isbn, campus=campus, person=person)
 
   db.session.add(new_submission)
   db.session.commit()
 
+  flash('Request submitted!')
   return redirect(url_for('main.submit'))
 
+# Allow the user to delete their own request
+@main.route('/delete/<int:id>')
+@login_required
+def delete_submission(id):
+  submission_to_delete = Submission.query.get(id)
+  db.session.delete(submission_to_delete)
+  db.session.commit()
+  flash('Request deleted!')
+  return redirect(url_for('main.submit'))
+
+# Admin can delete all requests
+@main.route('/delete_all')
+@login_required
+def delete_all():
+  db.session.query(Submission).delete()
+  db.session.commit()
+  return redirect(url_for('main.submit'))
 
--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_login import UserMixin
+from datetime import datetime
 from . import db
 
 class User(UserMixin, db.Model):
@@ -12,9 +13,10 @@ class Submission(UserMixin, db.Model):
   id = db.Column(db.Integer, primary_key=True)
   title = db.Column(db.String(1000))
   author = db.Column(db.String(1000))
-  publication =db.Column(db.Integer)
   isbn = db.Column(db.Integer)
   campus = db.Column(db.String(128))
+  date = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+  person = db.Column(db.Integer)
 
   def __repr__(self):
     return '<Submission {}>'.format(self.title)
